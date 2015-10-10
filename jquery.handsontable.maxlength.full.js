271270cde7fe3f4ad58f8f403954aef5af69c1be
marcioggs <marcioggs@gmail.com>
2015-10-10 13:40:59 -0300

    Refactored to use latest API and fix bugs.

--- jquery.handsontable.maxlength.full.js
+++ jquery.handsontable.maxlength.full.js
@@ -1,45 +1,27 @@
 /**
- * Handsontable max length plugin 1.0.0
+ * Handsontable max length plugin 2.0.0
  * Limit the max length of any cell of the Handsontable plugin.
  *
  * Márcio Gabriel
  * https://github.com/marcioggs
- * 
+ *
  * Usage:
- *	$("#sheetId").handsontable({
- *		(...),
- *		cells: function (row, col, prop) {
- *			var cellProperties = {};
- *			cellProperties.maxLength = 5;
- *			return cellProperties;
- *		}
- *  });
- * 
+ * 	new Handsontable(container, {
+ * 		(...)
+ * 		cells: function (row, col, prop) {
+ *   		this.maxLength = row + col;
+ *     	}
+ * 	});
+ *
  */
-(function ($) {
-	
-	/**
-	 * Add max length to a text area.
-	 * Implementation based on: http://stackoverflow.com/a/12131507/1026624
-	 * 
-	 * @param jqObj Object to add max length
-	 * @param maxLength Max length
-	 */
-	var _addMaxLengthTextArea = function(jqObj, maxLength) {
-		jqObj.unbind("input.handsontableMaxlength");
-		jqObj.unbind("propertychange.handsontableMaxlength");
-		
-		jqObj.bind("input.handsontableMaxlength propertychange.handsontableMaxlength", function() {
-			if ($(this).val().length > maxLength) {
-				$(this).val($(this).val().substring(0, maxLength));
-			}
-		});
-	};
+Handsontable.hooks.add('beforeChange', function(changes, source) {
 
-	(Handsontable.hooks || Handsontable.PluginHooks).add("afterGetCellMeta", function(row, col, cellProperties) {
-		if (typeof cellProperties.maxLength === "number") {
-			_addMaxLengthTextArea($(".handsontableInput"), cellProperties.maxLength);
-		}
-	});
-	
-})(jQuery);
+	var meta = this.getCellMeta(changes[0][0], changes[0][1]);
+	var text = changes[0][3];
+
+	if (typeof meta.maxLength == "number" &&
+			text.length > meta.maxLength) {
+				
+		changes[0][3] = text.substring(0, meta.maxLength);
+	}
+});
